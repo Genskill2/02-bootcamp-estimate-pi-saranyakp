27be84180ab92279218337df79f0f59bc12d27c0
saranyakp <84776715+saranyakp@users.noreply.github.com>
2021-06-02 12:50:33 +0530

    Update estimate.py

--- estimate.py
+++ estimate.py
@@ -1,6 +1,24 @@
 import math
+import random
 import unittest
 
+def wallis(n):
+    pi=1
+    for i in range(1,n):
+       pi*=((4*(i*2)/((4(i**2))-1)))
+    pi= pi*2
+    return (pi)  
+    
+def monte_carlo(n):
+    c=0
+    for i in range(0,n):
+       x=random.random()
+       y=random.random()
+       p_distance=x*2+y*2
+       if p_distance<=1:
+           c=c+1
+    pi= (4*(c/n))
+    return pi
 class TestWallis(unittest.TestCase):
     def test_low_iters(self):
         for i in range(0, 5):
